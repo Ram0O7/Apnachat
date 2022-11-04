5711cb9758581a883d40a021ee5f85c7092281e0
Ramkrishn <ram706860@gmail.com>
2022-11-04 20:09:15 +0530

    smalledits

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ io.on("connection", (socket) => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit("message", formatMessage(botName, "Welcome to ApnaChat!"));
+    socket.emit("message", formatMessage(botName, "Welcome to अपनाChat!"));
 
     // Broadcast when a user connects
     socket.broadcast
